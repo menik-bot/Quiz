bbc10cb1a2258c6bed32c2e5d690849ba57ae373
Ikechukwu Meniru <52763527+menik-bot@users.noreply.github.com>
2020-05-06 18:36:37 +0100

    Javascript 4

---
+++ JS/data.js
@@ -0,0 +1,46 @@
+let data = [{
+        id: 1,
+        question: "Who discovered the source of the River Niger?",
+        a: "Lander Brothers",
+        b: "Mungo Park",
+        c: "Lord Lugard",
+        d: "Sir Rawlings",
+        answer: 1
+    },
+    {
+        id: 2,
+        question: "Nigeria is situated in Western Africa and has a coastal border on which body of water?",
+        a: "Gulf of Aden",
+        b: "Gulf of Tadjoura",
+        c: "Gulf of Guinea",
+        d: "Gulf of Benin",
+        answer: 3
+    },
+    {
+        id: 3,
+        question: "Where is Abubakar Tafawa Balewa University?",
+        a: "Bauchi",
+        b: "Ife",
+        c: "Zaria",
+        d: "Kaduna",
+        answer: 1
+    },
+    {
+        id: 4,
+        question: "Who sang the song 'Send Down the Rain'?",
+        a: "Ras Kimono",
+        b: "Eva Edna Ogholi",
+        c: "Majek Fashek",
+        d: "Victor Uwaifo",
+        answer: 3
+    },
+    {
+        id: 5,
+        question: "Nigeria borders which of the following countries?",
+        a: "Ghana",
+        b: "Togo",
+        c: "Gabon",
+        d: "Benin",
+        answer: 4
+    },
+];
